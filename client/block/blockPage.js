297a38703449a54fe12569d4765c6d512f58bbde
Administrator <nedobryj@icloud.com>
2018-02-05 21:33:28 +0300

    blockPage - add symmary

--- client/block/blockPage.js
+++ client/block/blockPage.js
@@ -1,5 +1,8 @@
 Template.blockPage.helpers({
   transaction: function() {
     return Txs.find({block: this.hash});
+  },
+  timer: function() {
+    return moment.unix(this.time).format('MMMM Do YYYY, h:mm:ss a');
   }
 });
